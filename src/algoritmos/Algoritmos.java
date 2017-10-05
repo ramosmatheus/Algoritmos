039bf9ffc481ba78c5ab86d54a72a59258a00fc5
Aluno <Aluno@172.32.1.17>
2017-10-05 20:32:45 -0300

    Criação do Scanner.

--- src/algoritmos/Algoritmos.java
+++ src/algoritmos/Algoritmos.java
@@ -1,12 +1,16 @@
 
 package algoritmos;
 
+import java.util.Scanner;
+
 
 public class Algoritmos {
-
+    
 
     public static void main(String[] args) {
-
+        Scanner n = new Scanner(System.in);
+        
+        
     }
     
 }
